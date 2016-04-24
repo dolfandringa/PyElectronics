c93fbb71b4b777296c1bec80ffb7eea2b7553cb7
Dolf Andringa <dolfandringa@gmail.com>
2016-04-24 10:39:06 +0800

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='PyElectronics',
+      version='1.0',
+      description='Classes for aiding electronics calculations in python.',
+      author='Dolf Andringa',
+      author_email='dolfandringa@gmail.com',
+      url='http://dolfandringa.github.io/PyElectronics',
+      packages=['PyElectronics'],
+      package_dir={'': '../'},
+      )
